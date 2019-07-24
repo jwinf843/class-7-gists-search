ab3a1f61c2265a191c1a3cf4f01b0ad0af32b13a
Jordan Winfery <jwinf843@gmail.com>
2019-07-24 07:29:23 +0000

    Passing all tests by adding filter functionality

--- gist_search/search.py
+++ gist_search/search.py
@@ -1,7 +1,38 @@
-from gist_search.utils import get_gists
+# from gist_search.utils import get_gists
+from utils import get_gists
 
-
-def search_gists(username, description=None, file_name=None):
+def search_gists(username, description=None, file_name=None, ID=None, url=None):
     if not description and not file_name:
         print("At least one search parameter must be specified")
         return
+    
+    gists = get_gists(username)
+    results = []
+  
+    for gist in gists:
+        if description and description.lower() not in gist['description'].lower():
+            continue
+        
+        if ID and ID.lower() not in gist['id'].lower():
+            continue
+            
+        if url and url.lower() not in gist['url'].lower():
+            continue
+            
+        if file_name:
+            found = False
+            for fname in gist['files']:
+                if file_name.lower() in fname.lower():
+                    found = True
+                    break
+            if not found:
+                continue      
+        
+        results.append(gist)
+        
+    return results
+
+
+results = search_gists('santiagobasulto', description='Humble')
+
+print(results)
--- gist_search/utils.py
+++ gist_search/utils.py
@@ -4,8 +4,17 @@ BASE_URL = 'https://api.github.com/users/{username}/gists'
 
 
 def get_gists(username):
-    url = BASE_URL.format(username=username)
+    url = BASE_URL.format(username = username)
     resp = requests.get(url, params={'per_page': 100})
     if not resp.ok:
         return None
     return resp.json()
+
+
+if __name__ == '__main__':
+    gists = get_gists('santiagobasulto')
+
+    first_five = gists[:6]
+
+    for gist in first_five:
+        print("{:<40} | {}".format(gist['id'], gist['description']))
